8efb62511153a616c3e67564acee949a77639e91
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-14 16:11:54 +0300

    Handle Error with catchError and Throw Error

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { map } from "rxjs/operators";
-import { Subject } from "rxjs";
+import { map ,catchError} from "rxjs/operators";
+import { Subject,throwError } from "rxjs";
 
 @Injectable({
     providedIn:'root',
@@ -31,6 +31,8 @@ export class PostsService{
               }
             }
             return postsArray;
+          }),catchError(errorRes=>{
+            return throwError(errorRes);
           })
         );
     }
