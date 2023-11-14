d6df860007ee04f286dd97d12e4c901b645314e5
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-14 16:07:16 +0300

    Handle Error with Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit,OnDestroy {
   loadedPosts:Post[] = [];
   isFetching=false;
   error=null;
+  private errorSub:Subscription;
 
   constructor(private postService:PostsService) {}
 
   ngOnInit() {
+    this.errorSub=this.postService.error.subscribe(
+      errorMessage=>{
+        this.error=errorMessage;
+      }
+    );
     this.fetchPosts();
   }
 
@@ -49,5 +56,8 @@ export class AppComponent implements OnInit {
         this.error=error.message
       }
     );
+  }
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -2,18 +2,22 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Injectable({
     providedIn:'root',
 })
 export class PostsService{
 
+    error=new Subject<string>();
+
     constructor(private http :HttpClient){}
+
     createAndStorePosts(title:string,content:string){
         const postData:Post={title:title,content:content}
         this.http.post<{name:string}>('https://ng-complete-guide-abc63-default-rtdb.firebaseio.com/posts.json',postData).subscribe(responseData=>{
       console.log(responseData);
-    });
+    }),errorMessage=>{this.error.next(errorMessage)};
     }
 
     fetchPosts(){
