9e6f2ffe237eab5e79d6f1d9ee5290625ac239d0
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-15 18:59:30 +0300

    Mutliply Interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,13 +4,6 @@ import { tap } from "rxjs/operators";
 export class AuthInterceptorService implements HttpInterceptor{
     intercept(req:HttpRequest<any>,next:HttpHandler){
         const modifiedRequest= req.clone({headers:req.headers.append('Auth','xyz')})
-        console.log('Request it is on the way');
-        return next.handle(modifiedRequest).pipe(tap(event=>{
-            console.log(event);
-            if(event.type===HttpEventType.Response){
-                console.log('Response is arrived')
-                console.log(event.body);
-            }
-        }));
+        return next.handle(modifiedRequest);
     }
 }
