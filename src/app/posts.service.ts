9700ddaf3cc437c246b3a6c287150059c4312195
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-15 17:57:36 +0300

    Adding Observe

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { map ,catchError} from "rxjs/operators";
+import { map ,catchError, tap} from "rxjs/operators";
 import { Subject,throwError } from "rxjs";
 
 @Injectable({
@@ -15,7 +15,11 @@ export class PostsService{
 
     createAndStorePosts(title:string,content:string){
         const postData:Post={title:title,content:content}
-        this.http.post<{name:string}>('https://ng-complete-guide-abc63-default-rtdb.firebaseio.com/posts.json',postData).subscribe(responseData=>{
+        this.http.post<{name:string}>('https://ng-complete-guide-abc63-default-rtdb.firebaseio.com/posts.json',postData,
+        {
+          observe:'response'
+        }
+        ).subscribe(responseData=>{
       console.log(responseData);
     }),errorMessage=>{this.error.next(errorMessage)};
     }
@@ -41,7 +45,14 @@ export class PostsService{
     }
 
     deletePosts(){
-        return this.http.delete('https://ng-complete-guide-abc63-default-rtdb.firebaseio.com/posts.json');
+        return this.http.delete('https://ng-complete-guide-abc63-default-rtdb.firebaseio.com/posts.json',{
+          observe:'response'
+        }).pipe(tap(event=>{
+          console.log(event);
+          if(event.type===HttpEventType.Response){
+            console.log(event.body);
+          }
+        }));
     }
 
 }
